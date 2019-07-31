bfdce5e99a400b72cb4d3f515a84d2b203337d74
Damien Fleminks <damien@fleminks.com>
2019-07-31 20:22:09 +0100

    Task 2: Added status colors handler

--- force-app/main/default/lwc/soi_configVariables/soi_configVariables.js
+++ force-app/main/default/lwc/soi_configVariables/soi_configVariables.js
@@ -13,9 +13,9 @@ const athleteColumns = [
     { label: 'Medical Expiry', fieldName: 'SOI_Medical_Expiry__c', type: 'date', sortable : 'true' , initialWidth: 110 },
     { label: 'Card', fieldName: 'SOI_Membership_Card__c', type: 'boolean', initialWidth: 65 },
     { label: 'Sports', fieldName: 'mySports', type: 'text'},
-    { label: 'Status', fieldName: 'SOI_Status__c', type: 'text' , sortable : 'true'},
+    { label: 'Status', fieldName: 'SOI_Status__c', type: 'text' , sortable : 'true', cellAttributes:{ class: {fieldName:"SOI_Status__c"} }},
     //{ label: 'Club', fieldName: 'SOI_ClubName__c', type: 'text' , sortable : 'true'},
-    { label: 'Actions', type: 'button', initialWidth: 75, typeAttributes: { label: 'Edit', name: 'view_details', title: 'Click to View Details'}},
+    { label: 'Actions', type: 'button', initialWidth: 110, typeAttributes: { label: 'Edit', name: 'view_details', title: 'Click to View Details'}},
     {label: '', type: 'button', initialWidth: 110, typeAttributes:
                 { label: { fieldName: 'actionLabel'}, title: 'Click to Edit', name: 'delete-record', class: 'btn_next'}
             }
