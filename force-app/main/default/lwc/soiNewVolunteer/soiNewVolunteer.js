9a528abd5548a0bc832b0ee8d85c36f167713932
Damien Fleminks <damien@fleminks.com>
2019-08-19 08:55:39 +0100

    Task 9: Created pop-up, working on roles

---
+++ force-app/main/default/lwc/soiNewVolunteer/soiNewVolunteer.js
@@ -0,0 +1,150 @@
+/**
+ *   SoiNewVolunteer
+ *   18-08-2019
+ *   @ Damien Fleminks
+ */
+import { LightningElement,track,wire,api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import addRoles from '@salesforce/apex/SOI_VolunteerController.newRoles';
+import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+import CONTACT_OBJECT from '@salesforce/schema/Contact';
+import FIRSTNAME_FIELD from '@salesforce/schema/Contact.FirstName';
+import LASTNAME_FIELD from '@salesforce/schema/Contact.LastName';
+import COMMENTS from '@salesforce/schema/Contact.SOI_Comments__c';
+import CONS_ID from '@salesforce/schema/Contact.SOI_ConsID__c';
+import ACCOUNT from '@salesforce/schema/Contact.AccountId';
+
+
+export default class SoiNewVolunteer extends LightningElement {
+     // Track if modal is open or not
+     @track openmodel = false;
+
+     // Object and field variables for submit form
+     contactObject = CONTACT_OBJECT;
+     firstName = FIRSTNAME_FIELD;
+     lastName = LASTNAME_FIELD;
+     comments = COMMENTS;
+     athleteId = CONS_ID;
+     accountId =  ACCOUNT;
+     
+     @track submitDisabled = false;
+ 
+     @api userAccountId;
+
+     @api volunteerType;
+
+     //Get object information such as record types
+    @track objectInfo;
+    @wire(getObjectInfo, { objectApiName: CONTACT_OBJECT })
+    objectInfo;
+
+
+    //Return the record type Id for 'Athlete'
+    get recordTypeId() {
+        // Returns a map of record type Ids 
+        const rtis = this.objectInfo.data.recordTypeInfos;
+        return Object.keys(rtis).find(rti => rtis[rti].name === 'Volunteer');
+    }
+
+    @track _selected = [];
+
+    get options() {
+        return [
+            { label: 'Chairperson', value: 'Chairperson' },
+            { label: 'Manager', value: 'Manager' },
+            { label: 'Coach1', value: 'Coach1' },
+            { label: 'Coach2', value: 'Coach2' },
+            { label: 'Assistant', value: 'Assistant' },
+            { label: 'collector', value: 'collector' },
+            { label: 'teacher', value: 'teacher' },
+            { label: 'Treasurer', value: 'Treasurer' }        ];
+    }
+
+    get selected() {
+        return this._selected.length ? this._selected : false;
+    }
+
+    handleChange(e) {
+        this._selected = e.detail.value;
+    }
+
+    handleSubmit(event){
+        this.submitDisabled = true;
+        event.preventDefault();       // stop the form from submitting
+        const fields = event.detail.fields;
+        fields.AccountId = this.userAccountId;
+        fields.SOI_VolunteerType__c = this.volunteerType;
+        this.template.querySelector('lightning-record-edit-form').submit(fields);
+     }
+
+
+     /**
+      *  After form for new athlete is submitted, use the contactId of the newly created record to associate the assigned sports as related records 
+      * @param {*} event 
+      */
+    successHandle(event){
+        console.log('EXECUTE successHandle');
+        console.log(event.detail.id);
+        
+        if(this.selected){
+            console.log('his.selected) true');
+            console.log('event.detail.Id: ' + event.detail.id);
+            addRoles({ sportList: this.selected, contactId: event.detail.id})
+                .then(result => {
+                    console.log('event.detail successful');
+                    let toastInfo = {
+                        "myTitle" : "Succesfully created volunteer",
+                        "myMessage" : `Volunteer is created and roles have been added`,
+                        "variant" : "success"
+                    };
+                    this.triggerToast(toastInfo);
+                })
+                .catch(error => {
+                    console.log('ERROR on successHandle');
+                    console.log(error);
+                    let toastInfo = {
+                        "myTitle" : "Error adding volunteer roles",
+                        "myMessage" : `Volunteer succesfully created, but an error occurred adding the roles. Please try again or contact the administrator. Error message: ${error}`,
+                        "variant" : "error"
+                    };
+                    this.triggerToast(toastInfo);
+                });
+        }else{
+            let toastInfo = {
+                "myTitle" : "Succesfully created volunteer",
+                "myMessage" : `Volunteer is created, but no roles have been added yet`,
+                "variant" : "success"
+            };
+            this.triggerToast(toastInfo);
+        }   
+
+    }
+    
+
+
+    openmodal() {
+        this.openmodel = true
+    }
+    closeModal() {
+        this.openmodel = false
+    } 
+
+    /**
+     *  Function to trigger a toast pop-up message dynamically
+     * @param {*} toastInfo
+     */
+    triggerToast(toastInfo){
+        const evt = new ShowToastEvent({
+            title: toastInfo.myTitle,
+            message: toastInfo.myMessage,
+            variant: toastInfo.variant,
+        });
+        this.dispatchEvent(evt);
+        this.submitDisabled = false;
+        this.closeModal();
+        const selectedEvent = new CustomEvent('refreshApexEvent', { bubbles: true });
+        this.dispatchEvent(selectedEvent);
+    }
+
+
+}
